be60946074d5ce165f34e489d74b34261360d693
Ahmed-Elashmony <ahmedelashmony59@gmail.com>
2024-02-01 14:52:33 +0200

    stripe

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -9,11 +9,61 @@ import express from "express";
 //create order
 router.post("/", isAuth, orderController.createOrder);
 
-//webhook
-router.post(
+// //webhook
+// router.post(
+//   "/webhook",
+//   express.raw({ type: "application/json" }),
+//   orderController.orderWebhook
+// );
+
+const stripe = require("stripe")("sk_test_...");
+const app = express();
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_c5b60d7c9ef16b98396202380a20429943cfd363de56840fea0e635a8070e7b3";
+
+app.post(
   "/webhook",
   express.raw({ type: "application/json" }),
-  orderController.orderWebhook
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "checkout.session.async_payment_failed":
+        const checkoutSessionAsyncPaymentFailed = event.data.object;
+        // Then define and call a function to handle the event checkout.session.async_payment_failed
+        break;
+      case "checkout.session.async_payment_succeeded":
+        const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+        // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+        break;
+      case "checkout.session.completed":
+        const checkoutSessionCompleted = event.data.object;
+        // Then define and call a function to handle the event checkout.session.completed
+        break;
+      case "checkout.session.expired":
+        const checkoutSessionExpired = event.data.object;
+        // Then define and call a function to handle the event checkout.session.expired
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
 );
 
 export default router;
